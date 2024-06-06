0d878a06d0c825dec9cbf6e51c8e57f9ad184992
Hugo Soares <hugosoares.dev@gmail.com>
2024-06-06 11:04:41 -0300

    commit 001

---
+++ aula01.py
@@ -0,0 +1,11 @@
+n1 = 'ola'
+print(type(n1))
+n2 = 420
+print(type(n2))
+n3 = 4.20
+print(type(n3))
+n4 = False
+print(type(n4))
+
+nome = input('Digite seu nome? ')
+print(f'Ola {nome}, obrigado por me ajudar a aprender!')
