d69f169527dc28e2e48ae59ef6d3949a5e848c49
Hugo Soares <hugosoares.dev@gmail.com>
2024-06-20 21:27:26 -0300

    prova03

--- prova02.py
+++
@@ -1,6 +0,0 @@
-vel = int(input('informe a velocidade do veiculo em km/h '))
-mt = (vel - 80)*20.00
-if vel > 80:
-    print(f'voce foi multado por exceder a velocidade, transitando a {vel}km/h sendo a velocidade maxima permitida nesta via de 80km/h, a mulda sera no valor de R${mt:.2f} considerando a velocidade exedente\n Dirija com cuidado!')
-else:
-    print('velocidade permitida.\n respeite o transito, respeite a vida!')
