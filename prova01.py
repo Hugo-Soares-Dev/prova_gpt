cf38deb8415cdf286447dc65f3c1e413a481381c
Hugo Soares <hugosoares.dev@gmail.com>
2024-07-21 13:56:09 -0300

    prova

---
+++ prova01.py
@@ -0,0 +1,5 @@
+med = input('para calcular a area do retângulo primeiro digite:\n "Cm" para centimetros, "M" para metros ou "Km" para kilometros: ')
+base = float(input('digite  o valor da base: '))
+alt = float(input('agora digite a altura: '))
+
+print(f'levando em consideraçao a altura de {alt} e base de {base},\n a area do seu retângulo sera de: {base*alt:.2f}{med}²')
