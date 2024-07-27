794a0e6d595ebf153617cf1a3977d928b860ea36
Hugo Soares <hugosoares.dev@gmail.com>
2024-07-27 15:24:30 -0300

    condicionais

--- prova01.py
+++
@@ -1,5 +0,0 @@
-med = input('para calcular a area do retângulo primeiro digite:\n "Cm" para centimetros, "M" para metros ou "Km" para kilometros: ')
-base = float(input('digite  o valor da base: '))
-alt = float(input('agora digite a altura: '))
-
-print(f'levando em consideraçao a altura de {alt} e base de {base},\n a area do seu retângulo sera de: {base*alt:.2f}{med}²')
---
+++ prova02.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um numero: '))
+
+if num > 0:
+    print(f'Você digitou o numero "{num}", este numero é positivo')
+if num < 0:
+    print(f'Voce digitou o numero "{num}", este numero é negativo')
+if num == 0:
+ print(f'você digitou "{num}" Zero.')        
